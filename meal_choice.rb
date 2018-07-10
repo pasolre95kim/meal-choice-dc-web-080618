dd83d85a3250e13d3d44dbd946b363aed2839933
Michelle Kim <pasolre95kim@gmail.com>
2018-07-10 20:34:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,14 +2,10 @@
 # passed into it and defaults to meat.
 
 def food(meal_choice="meat")
-  puts "Your meal choice is #{meal_choice}!"
+  puts "The meal choice is #{meal_choice}!"
 end
 
-
-
-def choice(meal_choice="meat")
-    puts "The Meal Choice is #{meal_choice}!"
+def meal_choice(name="meat")
+  meal_choice = 
 end
 
-def meal_choice(name = "meat")
-  meal_choice = "#{name}"
